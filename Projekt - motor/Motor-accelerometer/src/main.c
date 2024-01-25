58b6410bf1997c66611c827a216706789da11fb0
JacobSig <77531247+JacobSig@users.noreply.github.com>
2024-01-25 14:23:55 +0100

    .

--- Projekt - motor/Motor-accelerometer/src/main.c
+++ Projekt - motor/Motor-accelerometer/src/main.c
@@ -3,17 +3,10 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "MPU6050_res_define.h"							/* Include MPU6050 register define file */
+#include "I2C_Master_H_file.h"	
 //#include <mpu6050.h>
-#include "i2cmaster.h"
-#include "uart.h"
-
-#define BAUDRATE 1200
-
-  uint8_t M1F = 0b00000101;
-  uint8_t M1B = 0b00001001;
-  uint8_t M2F = 0b00010010;
-  uint8_t M2B = 0b00100010;
-  uint8_t MO = 0b00000011;
+//#include "i2cmaster.h"
 
   uint8_t F = 0;
   uint8_t B = 0;
@@ -29,8 +22,6 @@ void init (void)
 //DDRC = 0b00111111;
 DDRB = 0b00001000;
 DDRD = 0b01101000;
-uart_init(UART_BAUD_SELECT(BAUDRATE, F_CPU));
-sei();
 // 0-1,2EN; 1-3,4EN; 2-1A; 3-2A; 4-3A; 5-4A;
 // setting up TIMER0
 TCCR0A = (1 << COM0A1 ) | (0 << COM0A0 ) | (1 << COM0B1 ) | (0 << COM0B0 )
@@ -49,55 +40,56 @@ ADCSRA = ( 1 << ADEN ) | ( 1 << ADSC )
         | ( 0 << ADATE ) | ( 0 << ADIF ) | ( 0 << ADIE )
         | ( 0 << ADPS2 ) | ( 1 << ADPS1 ) | ( 1 << ADPS0 );
 
-_delay_ms(1000);
-i2c_init();
 }
 
-// void Gyro_Init(){
-//   _delay_ms(150);
-//   i2c_start_wait(0xD0);
-//   i2c_write(SMPLRT_DIV);
-//   i2c_write(0x07);
-//   i2c_stop();
+void MPU6050_Init()										/* Gyro initialization function */
+{
+	_delay_ms(150);										/* Power up time >100ms */
+	I2C_Start_Wait(0xD0);								/* Start with device write address */
+	I2C_Write(SMPLRT_DIV);								/* Write to sample rate register */
+	I2C_Write(0x07);									/* 1KHz sample rate */
+	I2C_Stop();
 
-//   i2c_start_wait(0xD0);
-//   i2c_write(PWR_MGMT_1);
-//   i2c_write(0x01);
-//   i2c_stop();
+	I2C_Start_Wait(0xD0);
+	I2C_Write(PWR_MGMT_1);								/* Write to power management register */
+	I2C_Write(0x01);									/* X axis gyroscope reference frequency */
+	I2C_Stop();
 
-//   i2c_start_wait(0xD0);
-//   i2c_write(CONFIG);
-//   i2c_write(0x00);
-//   i2c_stop();
+	I2C_Start_Wait(0xD0);
+	I2C_Write(CONFIG);									/* Write to Configuration register */
+	I2C_Write(0x00);									/* Fs = 8KHz */
+	I2C_Stop();
 
-//   i2c_start_wait(0xD0);
-//   i2c_write(GYRO_CONFIG);
-//   i2c_write(0x18);
-//   i2c_stop();
+	I2C_Start_Wait(0xD0);
+	I2C_Write(GYRO_CONFIG);								/* Write to Gyro configuration register */
+	I2C_Write(0x18);									/* Full scale range +/- 2000 degree/C */
+	I2C_Stop();
 
-//   i2c_start_wait(0xD0);
-//   i2c_write(INT_ENABLE);
-//   i2c_write(0x01);
-//   i2c_stop();
-// }
+	I2C_Start_Wait(0xD0);
+	I2C_Write(INT_ENABLE);								/* Write to interrupt enable register */
+	I2C_Write(0x01);
+	I2C_Stop();
+}
 
-// void MPU_Start_Loc(){
-//   i2c_start_wait(0xD0);
-//   i2c_write(ACCEL_XOUT_H);
-//   i2c_rep_start(0xD1);
-// }
+void MPU_Start_Loc()
+{
+	I2C_Start_Wait(0xD0);								/* I2C start with device write address */
+	I2C_Write(ACCEL_XOUT_H);							/* Write start location address from where to read */ 
+	I2C_Repeated_Start(0xD1);							/* I2C start with device read address */
+}
 
-// void Read_RawValue(){
-//   MPU_Start_Loc();
-//   Acc_x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Acc_y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Acc_z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Temperature = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Gyro_x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Gyro_y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-//   Gyro_z= (((int)i2c_readAck()<<8) | (int)i2c_readNak());
-//   i2c_stop();
-// }
+void Read_RawValue()
+{
+	MPU_Start_Loc();									/* Read Gyro values */
+	Acc_x = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Acc_y = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Acc_z = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Temperature = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_x = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_y = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_z = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Nack());
+	I2C_Stop();
+}
 
 void ADC_X(){
   ADMUX = ( 0 << REFS1 ) | ( 1 << REFS0 )
@@ -116,7 +108,10 @@ void ADC_Y(){
 int main(void)
 {
   init();
-  //Gyro_Init();
+  I2C_Init();											/* Initialize I2C */
+	MPU6050_Init();	
+  float Xa,Ya,Za,t;
+	float Xg=0,Yg=0,Zg=0;
   OCR0A = 0;
   OCR0B = 0;
   OCR2A = 0;
@@ -136,6 +131,15 @@ int main(void)
     int16_t y_axis = 2*ADCH - 255;
     _delay_ms(1);
 
+    Read_RawValue();
+
+		Xa = Acc_x/16384.0;								/* Divide raw value by sensitivity scale factor to get real values */
+		Ya = Acc_y/16384.0;
+		Za = Acc_z/16384.0;
+		
+		Xg = Gyro_x/16.4;
+		Yg = Gyro_y/16.4;
+		Zg = Gyro_z/16.4;
     if (x_axis>=0)
     {
       F = x_axis;
